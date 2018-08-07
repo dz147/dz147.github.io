a9b7a21fa899b0d04bf7a2ce7a9aa059201c84ee
dz <1920098158@qq.com>
2018-08-07 09:35:20 +0800

    idea项目

---
+++ NewJava/src/com/dz/demo/Test.java
@@ -0,0 +1,35 @@
+package com.dz.demo;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("厚积而薄发 ！");
+        //itar (for循环)
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+        }
+
+        //iter (foreach循环)
+        for (String arg : args) {
+
+        }
+        //iti()
+        while (iterator.hasNext()) {
+            Object next =  iterator.next();
+
+        }
+
+    }
+    //pf()
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+    //prc
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    //prsf   private static final ;
+
+}
